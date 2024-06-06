457953fc8bd78f2bdc3138510a87e6ba0313e467
Claudia Zampini <lazampi1984@icloud.com>
2024-06-06 13:35:15 +0200

    sistemata parte grafica pag contatti

--- resources/views/contattaci.blade.php
+++ resources/views/contattaci.blade.php
@@ -1,39 +1,38 @@
 <x-layout title="Contattaci">
 
-    <div class="container">
-        <div class="row ">
-            <div class="col-12 col-md-6">
-                <h1 class="text-center p-2">CONTATTACI O VIENI A TROVARCI</h1>
+    <div class="container-fluid d-flex justify-content-center">
+        <div class="row mt-5">
+            <div class="col-12 mt-5">
+                <h1 class="text-center p-5 mt-5 title">CONTATTACI O VIENI A TROVARCI</h1>
             </div>
         </div>
     </div>
-
-    <div class="container-fluid d-flex mt-5 justify-content-center">
-        <div class="row vw-100 justify-content-center align-item-center">
-            <div class="col-12 col-md-4 text-white my-5 ms-5 justify-content-center align-item-center">
+    <div class="container-fluid d-flex  justify-content-center ">
+        <div class="row vw-100 justify-content-center align-item-center mx-5">
+            <div class="col-12 col-md-4 text-white my-5 justify-content-center align-item-center ">
                 <h3>DOVE SIAMO</h3>
-                <h4>Via S. Sebastiano 30r
-                <h4>16123 Genova</h4>
+                <h5>Via S. Sebastiano 30r
+                <h5>16123 Genova</h5>
                 <button class="btn link"><a
                         href="https://www.google.it/maps/dir//Via+di+S.+Sebastiano,+30,+16123+Genova+GE/@44.4093558,8.9329742,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x12d343e7d59304f1:0x66eed80cf7144a!2m2!1d8.9355545!2d44.409352?hl=it&entry=ttu"
-                        class="fs-4 link">Indicazioni</a><i class="bi bi-geo-alt-fill ms-3 fs-3"></i>
+                        class="fs-4 link">Indicazioni</a><i class="bi bi-geo-alt-fill fs-3"></i>
                 </button>
                 <h3 class="mt-5">MAIL & TELEFONO</h3>
                 <a href="" class="link">
-                <h4 class="link">info@griffegenova.com</h4>
+                <h5 class="link">info@griffegenova.com</h5>
                 </a>
-                <h4>+39 010 86 03 599</h4>
+                <h5>+39 010 86 03 599</h5>
                 <h3 class="mt-5">ORARI</h3>
-                <h4>Lunedì dalle 15:00 alle 18:00</h4>
-                <h4>Martedì dalle 10:00 alle 18:00e</h4>
-                <h4>Mercoledì dalle 10:00 alle 18:00</h4>
-                <h4>Giovedì dalle 10:00 alle 18:00</h4>
-                <h4>Venerdì dalle 10:00 alle 18:00</h4>
-                <h4>Sabato e Domenica CHIUSO</h4>
+                <h5>Lunedì dalle 15:00 alle 18:00</h5>
+                <h5>Martedì dalle 10:00 alle 18:00e</h5>
+                <h5>Mercoledì dalle 10:00 alle 18:00</h5>
+                <h5>Giovedì dalle 10:00 alle 18:00</h5>
+                <h5>Venerdì dalle 10:00 alle 18:00</h5>
+                <h5>Sabato e Domenica CHIUSO</h5>
             </div>
 
-            <div class="col-12 col-md-4 justify-content-center align-item-center my-5">
-                    <form class="p-5 shadow rounded formContatti" method="POST" action="{{route('contact.submit')}}">
+            <div class="col-12 col-md-6 justify-content-center align-item-center my-5 ">
+                    <form class="p-5 rounded formContatti" method="POST" action="{{route('contact.submit')}}">
                         @csrf
                         <div class="mb-3">
                             <label for="username" class="form-label">Inserisci il tuo nome completo</label>
@@ -48,7 +47,7 @@
                             <textarea name="message" class="form-control" id="message" cols="30" rows="10"></textarea>
                         </div>
 
-                        <button type="submit" class="btn btnForm">Submit</button>
+                        <button type="submit" class="btn btnForm">Invia</button>
                     </form>
                 </div>
             </div>
