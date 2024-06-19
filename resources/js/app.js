9a112841c8b23179f80dfe1bb4f9dfcce6ba8ec4
Claudia Zampini <lazampi1984@icloud.com>
2024-06-19 17:07:53 +0200

    aumentata velocità caroselli prodotti

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,16 @@
 import './bootstrap';
 import 'bootstrap';
-import 'swiper/css/bundle';
+// import 'swiper/css/bundle';
 // import './evento-scroll';
 // import './carosello';
 
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+// var swiper = new Swiper(".mySwiper", {
+//     pagination: {
+//         el: ".swiper-pagination",
+//         type: "fraction",
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,7 +11,8 @@
         <div class="row text-center">
             <div class="col-12 p-3 justify-content-between fs-2">
                 <p>Non sottovalutiamo l'importanza di trovare il colore perfetto per ogni progetto.
-                    Che tu stia rinnovando un ambiente interno o dando nuova vita a un mobile, il colore giusto può fare la differenza. Ecco perché offriamo un servizio di personalizzazione del colore:</p>
+                    Che tu stia rinnovando un ambiente interno o dando nuova vita a un mobile, il colore giusto può fare
+                    la differenza. Ecco perché offriamo un servizio di personalizzazione del colore:</p>
             </div>
             <div class="col-12">
                 <img class="img-fluid rounded-5" src="/media/cartellaColori.jpg" alt="">
@@ -22,10 +23,17 @@
         <div class="row text-center">
             <div class="col-12 p-3 justify-content-between fs-2">
                 <p class="mt-5 fs-1 text-center">Sistema Tintometrico Nice1975</p>
-                <p class="text-decoration-underline mt-5">Ampia Scelta di Colori:</p> <p> con il nostro sistema tintometrico, le possibilità sono infinite. Possiamo riprodurre qualsiasi colore tu abbia in mente, garantendo una perfetta corrispondenza con le tue aspettative</p>
-                <p class="text-decoration-underline mt-5">Campionatura:</p><p>avrai la possibilità di acquistare campioni da soli 100ml per non sprecare materiale</p>
-                <p class="text-decoration-underline mt-5">Velocità e Efficienza:</p><p>Il processo di miscelazione è rapido ed efficiente, permettendoti di ottenere il colore desiderato in tempi brevi</p>
-                <p class="text-decoration-underline mt-5">Supporto Personalizzato:</p><p>Il nostro team di esperti è a tua disposizione per aiutarti nella scelta del colore e per fornirti consulenza su come ottenere i migliori risultati per il tuo progetto</p>
+                <p class="text-decoration-underline mt-5">Ampia Scelta di Colori:</p>
+                <p> con il nostro sistema tintometrico, le possibilità sono infinite. Possiamo riprodurre qualsiasi
+                    colore tu abbia in mente, garantendo una perfetta corrispondenza con le tue aspettative</p>
+                <p class="text-decoration-underline mt-5">Campionatura:</p>
+                <p>avrai la possibilità di acquistare campioni da soli 100ml per non sprecare materiale</p>
+                <p class="text-decoration-underline mt-5">Velocità e Efficienza:</p>
+                <p>Il processo di miscelazione è rapido ed efficiente, permettendoti di ottenere il colore desiderato in
+                    tempi brevi</p>
+                <p class="text-decoration-underline mt-5">Supporto Personalizzato:</p>
+                <p>Il nostro team di esperti è a tua disposizione per aiutarti nella scelta del colore e per fornirti
+                    consulenza su come ottenere i migliori risultati per il tuo progetto</p>
             </div>
         </div>
     </div>
@@ -33,31 +41,38 @@
         <div class="row d-flex">
             <div class="col-12 justify-content-center align-content-center">
                 <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval="10000">
-                    <img class="caroselColori img-fluid" src="/media/colori/colori1.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="100">
+                            <img class="caroselColori img-fluid" src="/media/colori/colori1.jpg" class="d-block w-100"
+                                alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="1000">
+                            <img class="caroselColori img-fluid" src="/media/colori/colori2.jpg" class="d-block w-100"
+                                alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="caroselColori img-fluid" src="/media/colori/miscela.jpg" class="d-block w-100"
+                                alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="caroselColori img-fluid" src="/media/colori/palestra1.jpg" class="d-block w-100"
+                                alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="caroselColori img-fluid" src="/media/colori/muri1.jpg" class="d-block w-100"
+                                alt="...">
+                        </div>
                     </div>
-                    <div class="carousel-item" data-bs-interval="2000">
-                    <img class="caroselColori img-fluid" src="/media/colori/colori2.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="caroselColori img-fluid" src="/media/colori/miscela.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="caroselColori img-fluid" src="/media/colori/palestra1.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="caroselColori img-fluid" src="/media/colori/muri1.jpg" class="d-block w-100" alt="...">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon freccia" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                    <span class="carousel-control-next-icon freccia" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon freccia" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon freccia" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
         </div>
@@ -66,49 +81,64 @@
         <div class="row text-center">
             <div class="col-12 p-3 justify-content-between fs-2">
                 <p class="mt-5 fs-1 text-center">Idropitture e Smalti</p>
-                    <p> Trasforma i tuoi spazi interni con le nostre finiture per muro, metallo, legno, plastica e altre superfici specifiche. Le nostre vernici per interni sono facili da applicare, resistenti e garantiscono una copertura perfetta. Sono pitture all’acqua e a basso contenuto COV, etichettate A+</p>
-                    <p class="mt-5 fs-1 text-center">Resine e Microcementi</p>
-                    <p> Puoi scegliere i nostri rivestimenti in resina o microcemento disponibili in diverse tipologie di grane e realizzabili in qualsiasi colore, anche in abbinamento alle idropitture Nice1975.
-                    Possiamo rivestire superfici verticali e orizzontali, interne ed esterne. Ma anche realizzare vasche da bagno, piani di lavoro, mensole, consolle, intere cucine e persino divani.
-                    Inoltre dalla collaborazione con decoratrici dall’esperienza pluriennale è nata la proposta di resine extralusso, interamente decorate a mano e per questo uniche e irripetibili…..</p>
+                <p> Trasforma i tuoi spazi interni con le nostre finiture per muro, metallo, legno, plastica e altre
+                    superfici specifiche. Le nostre vernici per interni sono facili da applicare, resistenti e
+                    garantiscono una copertura perfetta. Sono pitture all’acqua e a basso contenuto COV, etichettate A+
+                </p>
+                <p class="mt-5 fs-1 text-center">Resine e Microcementi</p>
+                <p> Puoi scegliere i nostri rivestimenti in resina o microcemento disponibili in diverse tipologie di
+                    grane e realizzabili in qualsiasi colore, anche in abbinamento alle idropitture Nice1975.
+                    Possiamo rivestire superfici verticali e orizzontali, interne ed esterne. Ma anche realizzare vasche
+                    da bagno, piani di lavoro, mensole, consolle, intere cucine e persino divani.
+                    Inoltre dalla collaborazione con decoratrici dall’esperienza pluriennale è nata la proposta di
+                    resine extralusso, interamente decorate a mano e per questo uniche e irripetibili…..</p>
             </div>
         </div>
     </div>
-   <div class="container">
+    <div class="container">
         <div class="row d-flex">
             <div class="col-12 justify-content-center align-content-center">
                 <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval="10000">
-                    <img class="caroselColori img-fluid" src="/media/resina/pavResina.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="100">
+                            <img class="caroselColori img-fluid" src="/media/resina/pavResina.jpg" class="d-block w-100"
+                                alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="1000">
+                            <img class="caroselColori img-fluid" src="/media/resina/resinaDecAMano.jpg"
+                                class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="caroselColori img-fluid" src="/media/resina/resinaMuro.png"
+                                class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="caroselColori img-fluid" src="/media/resina/scalaResina.png"
+                                class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="caroselColori img-fluid" src="/media/resina/tavolo.jpg" class="d-block w-100"
+                                alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="caroselColori img-fluid" src="/media/resina/bagno.jpg" class="d-block w-100"
+                                alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="caroselColori img-fluid" src="/media/resina/scala2.jpg" class="d-block w-100"
+                                alt="...">
+                        </div>
                     </div>
-                    <div class="carousel-item" data-bs-interval="2000">
-                    <img class="caroselColori img-fluid" src="/media/resina/resinaDecAMano.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="caroselColori img-fluid" src="/media/resina/resinaMuro.png" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="caroselColori img-fluid" src="/media/resina/scalaResina.png" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="caroselColori img-fluid" src="/media/resina/tavolo.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="caroselColori img-fluid" src="/media/resina/bagno.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="caroselColori img-fluid" src="/media/resina/scala2.jpg" class="d-block w-100" alt="...">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon freccia" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                    <span class="carousel-control-next-icon freccia" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon freccia" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon freccia" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
         </div>
